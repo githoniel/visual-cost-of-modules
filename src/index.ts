e2e134c722210c84cb1679793ed84bb8083a12c1
githoniel <githoniel88@gmail.com>
2021-08-06 18:03:52 +0800

    fix: remove test code

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,1 @@
 import './App'
-
-import PM from '@/lib/PackageManager'
-
-const pm = new PM({
-  registry: 'https://registry.cnpmjs.org/'
-})
-
-pm.getMetadata('@gem-mine/cli').then((t) => {
-  console.log(t)
-})
